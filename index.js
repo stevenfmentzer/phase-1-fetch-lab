6c4e6962592c3c8dc7bbdab3fcfbfc3ccb653ab9
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-17 14:27:10 -0500

    adds code to index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => {
+      renderBooks(books)
+      });
+    }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
